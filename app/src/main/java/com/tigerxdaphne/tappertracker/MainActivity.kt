767424ba786e84e7965ce4238cb34174f62a159e
Tiger Oakes <contact@tigeroakes.com>
2020-04-24 18:00:33 -0700

    Update deps

--- app/src/main/java/com/tigerxdaphne/tappertracker/MainActivity.kt
+++ app/src/main/java/com/tigerxdaphne/tappertracker/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.tigerxdaphne.tappertracker
 
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        when {
+            // NFC not supported on the device
+            nfcAdapter == null -> {}
+            // NFC is turned off
+            !nfcAdapter.isEnabled -> {}
+            // Good to go
+            else -> {}
+        }
     }
 }
