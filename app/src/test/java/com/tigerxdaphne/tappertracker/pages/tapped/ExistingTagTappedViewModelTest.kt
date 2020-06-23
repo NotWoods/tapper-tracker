9bb0ddfc386c912184351f002f073b5b9f6049d1
Tiger Oakes <contact@tigeroakes.com>
2020-06-22 21:51:17 -0700

    Add more tests

--- app/src/test/java/com/tigerxdaphne/tappertracker/pages/tapped/ExistingTagTappedViewModelTest.kt
+++ app/src/test/java/com/tigerxdaphne/tappertracker/pages/tapped/ExistingTagTappedViewModelTest.kt
@@ -1,29 +1,25 @@
 package com.tigerxdaphne.tappertracker.pages.tapped
 
-import android.nfc.NfcAdapter
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tigerxdaphne.tappertracker.db.TappedRepository
 import com.tigerxdaphne.tappertracker.db.TappedTagModel
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
-import io.mockk.every
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.time.Clock
-import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
 
@@ -32,8 +28,7 @@ import java.time.ZoneOffset
 class ExistingTagTappedViewModelTest {
 
     private val testDispatcher = TestCoroutineDispatcher()
-    private val clock = Clock.fixed(Instant.EPOCH, ZoneOffset.UTC)
-    val exisitingTag = TappedTagModel(
+    private val existingTag = TappedTagModel(
         id = "123".toByteArray(),
         lastSet = LocalDate.of(2020, 5, 1),
         reminder = LocalDate.of(2020, 5, 3)
@@ -54,7 +49,35 @@ class ExistingTagTappedViewModelTest {
     }
 
     @Test
-    fun testStop() {
+    fun testStop() = testDispatcher.runBlockingTest {
+        val args = ExistingTagTappedFragmentArgs(existingTag)
+        val date = LocalDate.of(2020, 7, 1)
+        val clock = Clock.fixed(date.atStartOfDay(ZoneOffset.UTC).toInstant(), ZoneOffset.UTC)
+        val viewModel = ExistingTagTappedViewModel(args, clock, repository)
 
+        viewModel.stop()
+
+        coVerify {
+            repository.updateTag(
+                existingTag.copy(lastSet = date, isStopped = true)
+            )
+        }
+    }
+
+    @Test
+    fun testReset() = testDispatcher.runBlockingTest {
+        val args = ExistingTagTappedFragmentArgs(existingTag)
+        val date = LocalDate.of(2020, 7, 1)
+        val reminder = LocalDate.of(2020, 7, 3)
+        val clock = Clock.fixed(date.atStartOfDay(ZoneOffset.UTC).toInstant(), ZoneOffset.UTC)
+        val viewModel = ExistingTagTappedViewModel(args, clock, repository)
+
+        viewModel.reset()
+
+        coVerify {
+            repository.updateTag(
+                existingTag.copy(lastSet = date, reminder = reminder, isStopped = false)
+            )
+        }
     }
 }
